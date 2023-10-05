e0fe480f372bd606cb560ec43bfb45a945fa1a9a
guhpissai <gustavopissai3d@gmail.com>
2023-10-05 18:27:33 -0300

    Adicionando Javadoc para as classes Dto

--- src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -1,5 +1,8 @@
 package com.betrybe.museumfinder.dto;
 
+/**
+ * Um objeto de transferência de dados (DTO).
+ */
 public record MuseumCreationDto(String name, String description, String address,
                                 String collectionType, String subject, String url,
                                 com.betrybe.museumfinder.model.Coordinate coordinate) {
--- src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -1,6 +1,10 @@
 package com.betrybe.museumfinder.dto;
 
+/**
+ * Um objeto de transferência de dados (DTO).
+ */
 public record MuseumDto(Long id, String name, String description, String address,
                         String collectionType, String subject, String url,
                         com.betrybe.museumfinder.model.Coordinate coordinate) {
+
 }
